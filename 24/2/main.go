679e039c9740b20bf2b7889b8702493642baef8d
braedonwatkins <watkins.braedon@gmail.com>
2024-12-02 09:19:27 -0500

    day2 sol

---
+++ 24/2/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"aoc/utils"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func parseLines(data string) []string {
+	return strings.Split(data, "\n")
+}
+
+func abs(num int) int {
+	if num < 0 {
+		num *= -1
+	}
+
+	return num
+}
+
+func partOne(data string) int {
+	lines := parseLines(data)
+	// fmt.Printf("%q ", lines)
+
+	numSafe := 0
+	for _, line := range lines {
+		elements := strings.Split(line, " ")
+		if len(elements) < 2 {
+			continue
+		}
+
+		firstNum, _ := strconv.Atoi(elements[0])
+		secondNum, _ := strconv.Atoi(elements[1])
+
+		if secondNum > firstNum {
+			var i int
+			for i = 0; i < len(elements)-1; i++ {
+				curNum, _ := strconv.Atoi(elements[i])
+				nextNum, _ := strconv.Atoi(elements[i+1])
+				absDiff := abs(curNum - nextNum)
+
+				if absDiff > 3 || absDiff < 1 || nextNum < curNum {
+					break
+				}
+			}
+			// fmt.Println(i)
+			if i >= len(elements)-1 {
+				numSafe++
+			}
+		} else {
+			var i int
+			for i = 0; i < len(elements)-1; i++ {
+				curNum, _ := strconv.Atoi(elements[i])
+				nextNum, _ := strconv.Atoi(elements[i+1])
+				absDiff := abs(curNum - nextNum)
+
+				if absDiff > 3 || absDiff < 1 || nextNum > curNum {
+					break
+				}
+			}
+			// fmt.Println(i)
+			if i >= len(elements)-1 {
+				numSafe++
+			}
+
+		}
+	}
+
+	return numSafe
+}
+
+// FIXME: for some reason this fails the test but passes the input ??
+func partTwo(data string) int {
+	lines := parseLines(data)
+	// fmt.Printf("%q ", lines)
+
+	numSafe := 0
+	for _, line := range lines {
+		isProbDampened := false
+
+		elements := strings.Split(line, " ")
+		if len(elements) < 2 {
+			continue
+		}
+
+		firstNum, _ := strconv.Atoi(elements[0])
+		secondNum, _ := strconv.Atoi(elements[1])
+
+		if secondNum > firstNum {
+			var i int
+			for i = 0; i < len(elements)-1; i++ {
+				curNum, _ := strconv.Atoi(elements[i])
+				nextNum, _ := strconv.Atoi(elements[i+1])
+				absDiff := abs(curNum - nextNum)
+
+				if absDiff > 3 || absDiff < 1 || nextNum < curNum {
+					if !isProbDampened {
+						isProbDampened = true
+					} else {
+						break
+					}
+				}
+			}
+			// fmt.Println(i)
+			if i >= len(elements)-1 {
+				numSafe++
+			}
+		} else {
+			var i int
+			for i = 0; i < len(elements)-1; i++ {
+				curNum, _ := strconv.Atoi(elements[i])
+				nextNum, _ := strconv.Atoi(elements[i+1])
+				absDiff := abs(curNum - nextNum)
+
+				if absDiff > 3 || absDiff < 1 || nextNum > curNum {
+					if !isProbDampened {
+						isProbDampened = true
+					} else {
+						break
+					}
+				}
+			}
+			// fmt.Println(i)
+			if i >= len(elements)-1 {
+				numSafe++
+			}
+
+		}
+	}
+
+	return numSafe
+
+}
+
+func main() {
+	data := utils.FileParser("input.txt")
+	ans1 := partOne(data)
+	ans2 := partTwo(data)
+	fmt.Println(ans1, ans2)
+}
---
+++ 24/2/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"aoc/utils"
+	"testing"
+)
+
+func TestOne(t *testing.T) {
+	input := utils.FileParser("test.txt")
+	result := partOne(input)
+	expected := 2
+
+	if result != expected {
+		t.Errorf(
+			"expected result was %d, but got %d instead",
+			expected,
+			result,
+		)
+	}
+}
+
+func TestTwo(t *testing.T) {
+	input := utils.FileParser("test.txt")
+	result := partTwo(input)
+	expected := 4
+
+	if result != expected {
+		t.Errorf(
+			"expected result was %d, but got %d instead",
+			expected,
+			result,
+		)
+	}
+}
