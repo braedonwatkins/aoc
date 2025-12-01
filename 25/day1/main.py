0adba4d768c38b70d359eb19886cb80a8fd1dce7
braedonwatkins <watkins.braedon@gmail.com>
2025-12-01 18:55:52 -0500

    part two test pass

--- 25/day1/main.py
+++ 25/day1/main.py
@@ -1,6 +1,5 @@
 from utils import * 
 
-input = file_parse("test.txt")
 
 def partOne(input: str):
     print() #silly pytest, newlines are for kids!
@@ -31,12 +30,38 @@ def partOne(input: str):
     return total
 
 
-
-
-    return 3
-
 def partTwo(input: str):
     print()
+    lines = line_parse(input)
 
-    return -1
+    sign = None
+    cur = 50 
+    clicks = 0
+
+    # loop each line
+    for line in lines:
+        ## split first char 
+        if line[0] == 'L':
+            sign = -1
+        elif line[0] == 'R':
+            sign = 1
+        else:
+            raise Exception("halt and catch fire")
+
+        ## if L subtract, if R add
+        num = int(line[1:])
+        cur += sign*num
+
+
+        if cur == 0:
+            clicks += 1
+        while cur > 100:
+            cur -= 100
+            clicks += 1
+        while cur < 0:
+            cur += 100
+            clicks += 1
+
+
+    return clicks
 
--- 25/day1/test_parts.py
+++ 25/day1/test_parts.py
@@ -2,14 +2,14 @@ from day1.main import *
 from utils.main import *
 
 def test_one():
-    input_data = file_parse("test.txt")  
-    expected = 3
+    input_data = file_parse("input.txt")  
+    expected = 1100
     result = partOne(input_data)
     assert result == expected, f"expected result was {expected}, but got {result} instead"
 
 def test_two():
     input_data = file_parse("test.txt")
-    expected = -1
+    expected = 6
     result = partTwo(input_data)
     assert result == expected, f"expected result was {expected}, but got {result} instead"
 
