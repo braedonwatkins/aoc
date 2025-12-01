b5032f55917376102e69ead9721ececc8e1ba733
braedonwatkins <watkins.braedon@gmail.com>
2025-12-01 17:05:34 -0500

    unfinished 2024 day 5

---
+++ 24/5/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"aoc/utils"
+)
+
+func partOne(data string) {
+}
+
+func partTwo(data string) {
+}
+
+func main() {
+	_ = utils.FileParse("input.txt")
+	// ans := partOne(data)
+	// ans := partTwo(data)
+	// fmt.Println(ans)
+}
